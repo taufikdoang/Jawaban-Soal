7b6d3f2fa2fc328ca2afbb5f01b5f18532f34c84
taufikdoang <taufikbsi@gmail.com>
2022-07-20 23:35:25 +0700

    looping.js

--- looping.js
+++ looping.js
@@ -1,9 +1,22 @@
 // case 1
 
-var number = 10;
+// var number = 10;
 
-for(var i = 1; i <= number; i++) {
-    if (number % i == 0) {  
-       console.log(i)  
+// for(var i = 1; i <= number; i++) {
+//     if (number % i == 0) {  
+//        console.log(i)  
+//     }
+// }
+
+
+var num = 5;
+var temp = '';
+var chara = "#"
+
+for (var i = 0; i < num; i++) {
+    for (var j = 0; j <= i; j++) {
+        temp += chara
     }
+    console.log(temp)
+    temp = ""
 }
